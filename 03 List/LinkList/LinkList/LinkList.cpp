c2679e72b78286e7cb106872a4207f078eaac7bf
ZHANGMINLUN <allen5410323@gmail.com>
2022-03-30 09:35:22 +0800

    revise List with getNode() & -> operation

--- 03 List/LinkList/LinkList/LinkList.cpp
+++ 03 List/LinkList/LinkList/LinkList.cpp
@@ -1,46 +1,59 @@
-﻿// LinkList.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
-
+﻿using namespace std;
 #include <iostream>
-
-
 
 struct listNodeRef
 {
-    int data;
-    struct listNodeRef* linkRef;
+	int data;
+	struct listNodeRef* linkRef;
 };
 
+struct CircularList {
+	int data;
+	CircularList* llink;
+	CircularList* rlink;
+};
+
+listNodeRef* getNode() {
+
+	listNodeRef* node = (listNodeRef*)malloc(sizeof(listNodeRef));
+
+	return node;
+}
 
 int main()
 {
-    listNodeRef n1, n2, n3;
-    n1.data = 111;
-    n1.linkRef = &n2;
+	cout << "=======================Singly Linked List=========================" << endl;
+	listNodeRef* n1, * n2, * n3;
+	listNodeRef* nodes[3];
+	for (int i = 0; i < sizeof(nodes) / sizeof(nodes[0]); i++) {
+		nodes[i] = getNode();
+	}
 
-    n2.data = 222;
-    n2.linkRef = &n3;
+	nodes[0] = getNode();
+	nodes[0]->data = 111;
+	nodes[0]->linkRef = nodes[1];
 
-    n3.data = 333;
-    n3.linkRef = NULL;
+	nodes[1] = getNode();
+	nodes[1]->data = 222;
+	nodes[1]->linkRef = nodes[2];
 
-    printf("node1 data: %d\r\n", n1.data);
-    printf("node1 link: %p\r\n", n1.linkRef);
-    printf("node2 data: %d\r\n", n2.data);
-    printf("node2 link: %p\r\n", n2.linkRef);
-    printf("node3 data: %d\r\n", n3.data);
-    printf("node3 link: %p\r\n", n3.linkRef);
+	nodes[2] = getNode();
+	nodes[2]->data = 333;
+	nodes[2]->linkRef = NULL;
 
-    return 0;
+	printf("node1 data: %d\r\n", nodes[0]->data);
+	printf("node1 link: %p\r\n", nodes[0]->linkRef);
+	printf("node2 data: %d\r\n", nodes[1]->data);
+	printf("node2 link: %p\r\n", nodes[1]->linkRef);
+	printf("node3 data: %d\r\n", nodes[2]->data);
+	printf("node3 link: %p\r\n", nodes[2]->linkRef);
+
+	cout << "=======================Singly Linked List=========================" << endl;
+
+
+
+
+
+	return 0;
 }
 
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
