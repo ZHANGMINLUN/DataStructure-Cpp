366c66034038623d6e1a76ea3d935f7be259bebe
ZHANGMINLUN <allen5410323@gmail.com>
2022-04-06 09:12:27 +0800

    Stack initialize

---
+++ 04 Stack/Stack/Stack/Stack.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+typedef struct {
+	int ItemKey;
+}element;
+
+#define maxStackSize 100
+typedef struct {
+	element stack[maxStackSize];
+	int top;
+}stackADT;
+
+
+int main()
+{
+	//====================initialize====================//
+	stackADT s;
+	s.top = -1;
+
+
+}
+
