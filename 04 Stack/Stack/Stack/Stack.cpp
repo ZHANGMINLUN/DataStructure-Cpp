6cc316a701ea1ae8fe40c8cd7588f56bc96f9491
ZHANGMINLUN <allen5410323@gmail.com>
2022-04-06 09:45:42 +0800

    push pop

--- 04 Stack/Stack/Stack/Stack.cpp
+++ 04 Stack/Stack/Stack/Stack.cpp
@@ -10,13 +10,58 @@ typedef struct {
 	int top;
 }stackADT;
 
+bool isEmpty(stackADT s) {
+	if (s.top < 0)return true;
+	else return false;
+}
 
+bool isFull(stackADT s) {
+	if (s.top >= maxStackSize - 1)return true;
+	else return false;
+}
+
+void push(stackADT* s, element e) {
+	if (isFull(*s) == true) {
+		printf("Stack is Full!");
+		exit(1);
+	}
+	else {
+		//s->stack[++(s->top)] = e;
+		s->top++;
+		s->stack[s->top] = e;
+	}
+}
+
+//element pop(stackADT* s) {
+//	if(isEmpty())
+//}
+
+int maintest()
+{
+	int a = 10;
+	int* ptr1 = &a;
+	int** ptr2 = &ptr1;
+
+	printf("The address of a is %p\n", &a);
+	printf("The address of ptr1 is %p\n", &ptr1);
+	printf("The address of ptr2 is %p\n", &ptr2);
+
+	printf("The value of ptr1 is %p\n", ptr1);
+	printf("The value of ptr2 is %p\n", ptr2);
+	printf("The value of *ptr2 is %p\n", *ptr2);
+
+	printf("value of a: %d\n", a);
+	printf("value of *ptr1(point to variable a): %d\n", *ptr1);
+	printf("value of **ptr2(point to variable a): %d\n", **ptr2);
+
+	return 0;
+}
 int main()
 {
 	//====================initialize====================//
 	stackADT s;
 	s.top = -1;
-
+	maintest();
 
 }
 
