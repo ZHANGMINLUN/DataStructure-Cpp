de43db3a96a924c06b41611466c7c2d5816ca2f6
ZHANGMINLUN <allen5410323@gmail.com>
2022-04-18 20:44:22 +0800

    Tree struct

---
+++ 05 Queue/CircularQueue/CircularQueue/CircularQueue.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+
+typedef struct {
+    int itemKey;
+}element;
+
+#define maxCQueueSize 5
+typedef struct {
+    element cQueue[maxCQueueSize];
+    int front;
+    int rear;
+}circularQueueADT;
+
+bool isFrontEqRear(circularQueueADT* cQ) {
+    if (cQ->front == cQ->rear)return true;
+    else return false;
+}
+
+//bool isFull(circularQueueADT* cQ) {
+//    if (cQ->front == cQ->rear)return true;
+//    else return false;
+//}
+//
+//bool isEmptyOrFull(circularQueueADT* cQ) {
+//    cQ->rear = (cQ->rear + 1) % maxCQueueSize;
+//    if (cQ->front == cQ->rear)return true;
+//    else return false;
+//}
+
+
+void addCQueue(circularQueueADT* cQ, element e) {
+    cQ->rear = (cQ->rear + 1) % maxCQueueSize;
+    if (isFrontEqRear(cQ)) {
+        printf("Circular Queue is Full!");
+        exit(1);
+    }
+    else
+    {
+        cQ->cQueue[cQ->rear] = e;
+    }
+}
+
+element deleteCQueue(circularQueueADT* cQ) {
+    if (isFrontEqRear(cQ)) {
+        printf("Circular Queue is Empty!");
+        exit(1);
+    }
+    else {
+        cQ->front = (cQ->front + 1) % maxCQueueSize;
+        return cQ->cQueue[++cQ->front];
+    }
+}
+
+void printCQueue(circularQueueADT* cQ) {
+    for (int i = 0; i < maxCQueueSize; i++) {
+        printf("| Q[%d]: %d |—", i, cQ->cQueue[i]);
+    }
+}
+
+int main()
+{
+    circularQueueADT cQ;
+    cQ.front = 0;
+    cQ.rear = 0;
+    element e;
+
+    for (int i = 0; i < 20; i++) {
+        e.itemKey = i + 1;
+        addCQueue(&cQ, e);
+        printf("\r\n");
+        printCQueue(&cQ);
+    }
+
+}
+
+
---
+++ 06 Tree/Tree/Tree/Tree.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+
+typedef struct nodestr* bt_pointer;
+
+typedef struct nodestr {
+    int data;
+    bt_pointer lChild, rChild;
+}treenode;
+
+
+
+int main()
+{
+    return 0;
+}
+
+
